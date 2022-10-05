a7872cbb082cd19fdb022d0e58770f427c812d11
Royer Corentin <royc@elcaNet.local>
2022-10-05 15:31:01 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+import os
+
+_PATH_ROOT = os.path.realpath(os.path.dirname(__file__))
+
+def _load_requirements(path_dir: str, file_name: str = "requirements.txt", comment_char: str = "#") -> list[str]:
+    """Load requirements from a file.
+    >>> _load_requirements(_PATH_ROOT)
+    ['numpy...', 'torch..."]
+    """
+    with open(os.path.join(path_dir, file_name)) as file:
+        lines = [ln.strip() for ln in file.readlines()]
+    reqs = []
+    for ln in lines:
+        # filer all comments
+        if comment_char in ln:
+            char_idx = min(ln.index(ch) for ch in comment_char)
+            ln = ln[:char_idx].strip()
+        # skip directly installed dependencies
+        if ln.startswith("http") or ln.startswith("git") or ln.startswith("-r") or "@" in ln:
+            continue
+        if ln:  # if requirement is not empty
+            reqs.append(ln)
+    return reqs
+
+BASE_REQUIREMENTS = _load_requirements(path_dir=_PATH_ROOT, file_name="requirements.txt")
+
+if __name__ == "__main__":
+    setup(name='NLP_Lib',
+        version='1.0',
+        description='Various tools for NLP with PyTorch',
+        author='Corentin Royer',
+        url='https://github.com/corentin-ryr/NLP_lib',
+        install_requires=BASE_REQUIREMENTS,
+        )
